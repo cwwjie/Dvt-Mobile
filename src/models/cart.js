563d745172eb7348b2e11eb3b21a973796847ce2
cwwjie <454766952@qq.com>
2018-03-16 15:39:44 +0800

    add BuyWaySheet

--- src/models/cart.js
+++ src/models/cart.js
@@ -3,6 +3,31 @@ let ShoppingCart = {
     'namespace': 'cart',
   
     'state': {
+      'buyWay': '', // 购买方式 度假村自取 快递 潜游时光公司自取
+      'shoppingCartList': [ // 购物车列表
+        // {
+        //   'isSelected': false, // 是否被选中
+        // }
+      ]
+    },
+
+    'reducers': {
+      addEquipment(state, data) {
+        let myShoppingCartList = state.shoppingCartList.concat([]);
+
+        myShoppingCartList.push(data.equipmentItem);
+        return {
+          ...state,
+          'shoppingCartList': myShoppingCartList
+        }
+      },
+
+      changeBuyWay(state, data) {
+        return {
+          ...state,
+          'buyWay': data.buyWay
+        }
+      }
     },
   },
 
--- src/routes/Equipment/Detail/index.js
+++ src/routes/Equipment/Detail/index.js
@@ -1,19 +1,32 @@
 import React, {Component} from 'react';
 import { connect } from 'dva';
 import { routerRedux } from 'dva/router';
-import { Toast, Carousel, WhiteSpace, List, WingBlank, Steps } from 'antd-mobile';
+import { Toast, Carousel, List, Steps, ActionSheet, Modal } from 'antd-mobile';
 
 import MyNavBar from './../../../components/MyNavBar/index';
 import config from './../../../config';
 import onMenuShare from './../../../utils/weixin-onMenuShare';
+
+const isIPhone = new RegExp('\\biPhone\\b|\\biPod\\b', 'i').test(window.navigator.userAgent);
+let wrapProps;
+if (isIPhone) {
+  wrapProps = {
+    onTouchStart: e => e.preventDefault(),
+  };
+}
 
 class EquipmentDetail extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      'carousel': [{'src': null, 'width': null}]
+      'carousel': [{'src': null, 'width': null}],
+      'equipmentItem': {
+        'name': 'GoPro运动摄像机遥控器Smart Remote'
+      }
     }
+
+    this.buyWaySheetList = ['快递', '度假村自取', '潜游时光公司自取', '取消'];
   }
 
   jumpToShoppingCart() {
@@ -21,11 +34,56 @@ class EquipmentDetail extends Component {
     this.props.dispatch(routerRedux.push('/user/cart/index'));
   }
 
-  jumpToShoppingConfirm() {
-    this.props.dispatch(routerRedux.push('/user/cart/confirm'));
+  componentDidMount() {
   }
 
-  componentDidMount() {
+  addToShoppingCartAction() {
+    const shoppingCartList = this.props.shoppingCartList;
+
+    if (shoppingCartList.length === 0) {
+      this.showBuyWaySheet();
+    } else {
+      this.dispatchToShoppingCart();
+    }
+  }
+
+  dispatchToShoppingCart() {
+    const _this = this;
+    this.props.dispatch({ 
+      'type': 'cart/addEquipment', 
+      'equipmentItem': this.state.equipmentItem
+    });
+
+    Modal.alert('添加成功', <div>恭喜你,成功添加至购物车.</div>, [
+      { 'text': '查看购物车', 'onPress': _this.jumpToShoppingCart.bind(this), 'style': {'color': '#108ee9'} },
+      { 'text': '返回', 'style': {'color': '#000'} }
+    ]);
+  }
+
+  showBuyWaySheet() {
+    const _this = this;
+    const cancelButtonIndex = this.buyWaySheetList.length - 1;
+
+    ActionSheet.showActionSheetWithOptions({
+      'options': this.buyWaySheetList,
+      'cancelButtonIndex': cancelButtonIndex,
+      'title': '请选择收货方式',
+      'maskClosable': true,
+      'data-seed': 'logId',
+      wrapProps,
+    }, buttonIndex => {
+      if (cancelButtonIndex !== buttonIndex) {
+        _this.props.dispatch({ 
+          'type': 'cart/changeBuyWay', 
+          'buyWay': _this.buyWaySheetList[buttonIndex]
+        });
+        _this.dispatchToShoppingCart();
+      }
+    });
+  }
+
+  hiddenBuyWaySheet() {
+    this.setState({'isBuyWaySheetShow': false});
   }
 
   render() {
@@ -53,6 +111,9 @@ class EquipmentDetail extends Component {
           </div>
         ))}</Carousel>
 
+        <div className="Equipment-buyWaySheet">
+        </div>
+
         <div className='detail-bottom'>
           <div className='bottom-left'>
             <div>
@@ -60,6 +121,7 @@ class EquipmentDetail extends Component {
             </div>
           </div>
           <div className='bottom-mid'
+            onClick={this.addToShoppingCartAction.bind(this)}
           >加入购物车</div>
           <div className='bottom-right'
             onClick={this.jumpToShoppingCart.bind(this)}
@@ -71,7 +133,8 @@ class EquipmentDetail extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  'isLogin': state.user.isLogin
+  'buyWay': state.cart.buyWay,
+  'shoppingCartList': state.cart.shoppingCartList,
 })
 
 export default connect(mapStateToProps)(EquipmentDetail);
